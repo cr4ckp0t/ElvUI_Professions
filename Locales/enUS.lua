1c1c708cc1c47ecbdc77b6973b0d9c57923c0c19
Adam Koch <me@adamko.ch>
2020-02-16 13:57:39 -0500

    Localization improvements.

--- Locales/enUS.lua
+++ Locales/enUS.lua
@@ -9,20 +9,4 @@ local AceLocale = LibStub:GetLibrary("AceLocale-3.0")
 local L = AceLocale:NewLocale("ElvUI_Professions", "enUS", true, debug)
 if not L then return end
 
-L["Left Click:"] = true
-L["Mining"] = true
-L["No Profession"] = true
-L["Open "] = true
-L["Open Archaeology"] = true
-L["Open Cooking"] = true
-L["Plugins by |cff9382c9Crackpotx|r"] = true
-L["Professions Datatext"] = true
-L["Professions"] = true
-L["Right Click:"] = true
-L["Select which profession to display."] = true
-L["Shift + Left Click:"] = true
-L["Shift + Right Click:"] = true
-L["Show Hint"] = true
-L["Show the hint in the tooltip."] = true
-L["Smelting"] = true
-L["Thanks for using and supporting my work!  -- |cff9382c9Crackpotx|r\n\n|cffff0000If you find any bugs, or have any suggestions for any of my addons, please open a ticket at that particular addon's page on CurseForge."] = true
+--@localization(locale="enUS", format="lua_additive_table", same-key-is-true=true, escape-non-ascii=true, handle-unlocalized="english")@
