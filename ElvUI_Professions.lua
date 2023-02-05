1704e69d382e1860f75ca0908adba981ef279cc8
Azilroka <darktemplar9900@yahoo.com>
2023-02-05 15:56:11 -0600

    Fix whitespaces

--- ElvUI_Professions.lua
+++ ElvUI_Professions.lua
@@ -28,17 +28,17 @@ local function GetProfessionName(prof)
 end
 
 local function sortedPairs(t, f)
-    local a = {}
-    for n in pairs(t) do tinsert(a, n) end
-    sort(a, f)
-    local i = 0
-    local iter = function()
-        i = i + 1
-        if a[i] == nil then return nil
-        else return a[i], t[a[i]]
-        end
-     end
-     return iter
+	local a = {}
+	for n in pairs(t) do tinsert(a, n) end
+	sort(a, f)
+	local i = 0
+	local iter = function()
+		i = i + 1
+		if a[i] == nil then return nil
+		else return a[i], t[a[i]]
+		end
+	end
+	return iter
 end
 
 local function OnEvent(self)
